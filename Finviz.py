e0266f3455558e7087fa0eb725bb2fa0aae2046e
bbalsam <43484084+bbalsam@users.noreply.github.com>
2023-02-23 10:54:46 -0600

    Create Finviz.py

---
+++ Finviz.py
@@ -0,0 +1,9 @@
+import finviz
+
+finviz.get_stock('AAPL')
+print(finviz.get_stock('AAPL'))
+finviz.get_insider('АAPL')
+print(finviz.get_insider('АAPL'))
+#finviz.get_news('AAPL')
+
+#finviz.get_analyst_price_targets('AAPL')
